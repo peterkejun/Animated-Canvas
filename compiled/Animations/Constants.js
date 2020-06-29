33428c766f1ab4f35ecc09b55984d36fd5617622
Pierre Beasley <peter.ke@yahoo.com>
2020-06-28 22:24:54 -0400

    fixed .gitignore to include compiled js code and webpack dist

--- compiled/Animations/Constants.js
+++
@@ -1,7 +0,0 @@
-export var AnimationState;
-(function (AnimationState) {
-    AnimationState[AnimationState["IDLE"] = 0] = "IDLE";
-    AnimationState[AnimationState["RUNNING"] = 1] = "RUNNING";
-    AnimationState[AnimationState["FINISHED"] = 2] = "FINISHED";
-})(AnimationState || (AnimationState = {}));
-;
